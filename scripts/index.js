83935bf4353e1f9b6558f04dc91f8b0c7f333584
Stephen Gichia <stephengichia85@gmail.com>
2025-05-06 11:27:01 +0300

    move addToCart into cart.js

--- scripts/index.js
+++ scripts/index.js
@@ -31,7 +31,7 @@
 
 // loop through the above array
 
-import {cart} from '../data/cart.js';
+import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 
 let productsHTML = '';
@@ -95,35 +95,20 @@ products.forEach((product) => {
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 
-// make the website interactive
+function updateCartQuantity(){
+  let cartQuantity = 0;
 
+  cart.forEach((cartItem)=>{
+    cartQuantity += cartItem.quantity
+  });
+
+  document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+}
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
     const productId = button.dataset.productId;
-
-    let matchingItem;
-
-    cart.forEach((item)=>{
-      if (productId === item.productId){
-        matchingItem = item
-      }
-    });
-
-    if (matchingItem){
-      matchingItem.quantity += 1;
-    } else {
-      cart.push({
-        productId:productId,
-        quantity: 1
-      });
-    }
-
-    let cartQuantity = 0;
-
-    cart.forEach((item)=>{
-      cartQuantity += item.quantity
-    });
-
-    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+    addToCart(productId);
+    updateCartQuantity();
+  
   });
 });
