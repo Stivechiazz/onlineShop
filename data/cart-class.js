924b308b01798c66c08827856bfc37a6c29b17b8
Stephen Gichia <stephengichia85@gmail.com>
2025-05-13 10:18:41 +0300

    create class version of cart

---
+++ data/cart-class.js
@@ -0,0 +1,83 @@
+class Cart{
+    cartitems;
+    localStorageKey;
+
+
+    constructor(localStorageKey){
+        this.localStorageKey = localStorageKey;
+        this.loadFromStorage();
+
+    }
+
+    loadFromStorage(){
+this.cartitems = JSON.parse(localStorage.getItem(this.localStorageKey))
+
+if(!this.cartitems) {
+this.cartitems = 
+[{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+    deliveryOptionId: '1'
+},{
+    productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1,
+    deliveryOptionId: '2'
+}];
+}
+} 
+
+saveToStorage(){
+  localStorage.setItem(this.localStorageKey,JSON.stringify(this.cartitems));
+}
+
+
+addToCart(productId){
+    let matchingItem;
+  
+      this.cartitems.forEach((cartItem)=>{
+        if (productId === cartItem.productId){
+          matchingItem = cartItem;
+        }
+      });
+  
+      if (matchingItem){
+        matchingItem.quantity += 1;
+      } else {
+        this.cartitems.push({
+          productId:productId,
+          quantity: 1,
+          deliveryOptionId: '1'
+        });
+      }
+
+      this.saveToStorage();
+  }
+
+updateDeliveryOption(productId, deliveryOptionId){
+let matchingItem;
+
+this.cartitems.forEach((cartItem)=>{
+    if (productId === cartItem.productId){
+    matchingItem = cartItem;
+    }
+});
+
+matchingItem.deliveryOptionId = deliveryOptionId
+
+this.saveToStorage();
+}
+
+}
+
+
+const cart = new Cart('cart-oop');
+const businessCart = new Cart('cart-business');
+
+
+console.log(cart);
+console.log(businessCart);
+
+
+
+
+  
